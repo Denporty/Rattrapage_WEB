33611e7048240dc9010b7411ca749b562ad478c3
Denporty <55090852+Denporty@users.noreply.github.com>
2021-02-02 12:25:12 +0100

    Fix addMethod() for all fields

--- assets/components/TodoTable.js
+++ assets/components/TodoTable.js
@@ -10,11 +10,24 @@ import AddIcon from '@material-ui/icons/Add';
 function TodoTable(){
 
     const context = useContext(TodoContext);  
-    const [addTodo, setAddTodo] = useState('');      
+    const [addTodo, setAddTodo] = useState('');
+    const [addTodo1, setAddTodo1] = useState('');  
+    const [addTodo2, setAddTodo2] = useState('');  
+    const [addTodo3, setAddTodo3] = useState('');  
+    const [addTodo4, setAddTodo4] = useState('');  
+    const [addTodo5, setAddTodo5] = useState('');  
+    const [addTodo6, setAddTodo6] = useState('');  
+    const [addTodo7, setAddTodo7] = useState('');  
+    const [addTodo8, setAddTodo8] = useState('');  
+    const [addTodo9, setAddTodo9] = useState('');  
+    const [addTodo10, setAddTodo10] = useState('');  
+    const [addTodo11, setAddTodo11] = useState('');  
+    const [addTodo12, setAddTodo12] = useState('');  
+    const [addTodo13, setAddTodo13] = useState('');       
 
         return (
             <form onSubmit={(event) => {
-                context.createTodo(event, {name: addTodo});
+                context.createTodo(event, {name: addTodo, company: addTodo1, customer: addTodo2, customerPostalCode: addTodo3, companyPostalCode: addTodo4, customerPhoneNumber: addTodo5, companyPhoneNumber: addTodo6, firstField: addTodo7, firstPrice: addTodo8, secondField: addTodo9, secondPrice: addTodo10, thirdField: addTodo11, thirdPrice: addTodo12, total: addTodo13});
                 }}>
             <Table>
                 <TableHead>
@@ -31,54 +44,59 @@ function TodoTable(){
                                 }} label="Intitulé du devis"/>
                                                        </TableCell>
                             <TableCell>
-                            <TextField value={addTodo} onChange={(event) => {
-                                setAddTodo(event.target.value)
+                            <TextField value={addTodo1} onChange={(event) => {
+                                setAddTodo1(event.target.value)
                                 }} label="Societe émettrice"/> 
-                                <TextField value={addTodo} onChange={(event) => {
-                                setAddTodo(event.target.value)
+                                <TextField value={addTodo2} onChange={(event) => {
+                                setAddTodo2(event.target.value)
                                 }} label="Client"/> 
                             </TableCell>
                             <TableCell>
-                            <TextField value={addTodo} onChange={(event) => {
-                                setAddTodo(event.target.value)
+                            <TextField value={addTodo3} onChange={(event) => {
+                                setAddTodo3(event.target.value)
                                 }} label="Code postal du client"/> 
-                            <TextField value={addTodo} onChange={(event) => {
-                                setAddTodo(event.target.value)
+                            <TextField value={addTodo4} onChange={(event) => {
+                                setAddTodo4(event.target.value)
                                 }} label="Code postal de la societe émettrice"/> 
                             </TableCell>
                             <TableCell>
-                            <TextField value={addTodo} onChange={(event) => {
-                                setAddTodo(event.target.value)
+                            <TextField value={addTodo5} onChange={(event) => {
+                                setAddTodo5(event.target.value)
                                 }} label="Numéro de téléphone du client"/> 
-                            <TextField value={addTodo} onChange={(event) => {
-                                setAddTodo(event.target.value)
+                            <TextField value={addTodo6} onChange={(event) => {
+                                setAddTodo6(event.target.value)
                                 }} label="Numéro de téléphone de la societe émettrice"/> 
                             </TableCell>
                             <TableCell>
-                            <TextField value={addTodo} onChange={(event) => {
-                                setAddTodo(event.target.value)
+                            <TextField value={addTodo7} onChange={(event) => {
+                                setAddTodo7(event.target.value)
                                 }} label="Premiere ligne"/> 
-                            <TextField value={addTodo} onChange={(event) => {
-                                setAddTodo(event.target.value)
+                            <TextField value={addTodo8} onChange={(event) => {
+                                setAddTodo8(event.target.value)
                                 }} label="Prix premiere ligne"/> 
                             </TableCell>
                             <TableCell>
-                            <TextField value={addTodo} onChange={(event) => {
-                                setAddTodo(event.target.value)
+                            <TextField value={addTodo9} onChange={(event) => {
+                                setAddTodo9(event.target.value)
                                 }} label="Seconde ligne"/> 
-                            <TextField value={addTodo} onChange={(event) => {
-                                setAddTodo(event.target.value)
+                            <TextField value={addTodo10} onChange={(event) => {
+                                setAddTodo10(event.target.value)
                                 }} label="Prix seconde ligne"/> 
                             </TableCell>
                             <TableCell>
-                            <TextField value={addTodo} onChange={(event) => {
-                                setAddTodo(event.target.value)
+                            <TextField value={addTodo11} onChange={(event) => {
+                                setAddTodo11(event.target.value)
                                 }} label="Troisieme ligne"/> 
-                            <TextField value={addTodo} onChange={(event) => {
-                                setAddTodo(event.target.value)
+                            <TextField value={addTodo12} onChange={(event) => {
+                                setAddTodo12(event.target.value)
                                 }} label="Prix troisieme ligne"/> 
                             </TableCell>
                         <TableCell>
+                            <TableCell>
+                        <TextField value={addTodo13} onChange={(event) => {
+                                setAddTodo13(event.target.value)
+                                }} label="Total"/> 
+                            </TableCell>
                             
                             <IconButton type="submit">
                                 <AddIcon/>
